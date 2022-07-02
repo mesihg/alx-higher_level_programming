a4970d8f2a2b66df28567e4ba3d7bf361a311551
Mesih Grmay <mesihgrmay@yahoo.com>
2022-07-01 23:51:27 -0700

    Task 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-
 def print_list_integer(my_list=[]):
     """Prints contents of my_list list
     Args:
        my_list: list
     """
     for i in my_list:
-        print("{}".format(i))
+        print("{:d}".format(i))
