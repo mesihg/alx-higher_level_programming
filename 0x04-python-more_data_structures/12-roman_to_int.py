2f18f7084fb97d460340d2d5075a22932f5b4e39
Mesih Grmay <mesihgrmay@yahoo.com>
2022-07-06 12:46:34 -0700

    Task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    if not roman_string:
+    if not roman_string or None:
         return 0
     sum = 0
     i = 0
