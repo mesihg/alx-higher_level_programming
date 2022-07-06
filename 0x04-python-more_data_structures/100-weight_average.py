f9b1fbcbda88d0aa7a4a9c7cce63275827703734
Mesih Grmay <mesihgrmay@yahoo.com>
2022-07-06 13:09:04 -0700

    Task 14

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    score_sum = 0
+    weight_sum = 0
+    for val in my_list:
+        score_sum += val[0] * val[1]
+        weight_sum += val[1]
+    return score_sum/weight_sum
