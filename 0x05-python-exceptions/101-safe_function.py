b4b6cf8be4a16630e655ec520ea3dca2dddb6e25
Mesih Grmay <mesihgrmay@yahoo.com>
2022-07-18 13:00:43 -0700

    Task 8

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -6,6 +6,6 @@ import sys
 def safe_function(fct, *args):
     try:
         return fct(*args)
-    except TypeError:
+    except Exception:
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return None
