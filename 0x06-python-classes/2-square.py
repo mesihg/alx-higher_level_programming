8fe3d20ac48327eb256e9c325a457e8d56863216
Mesih Grmay <mesihgrmay@yahoo.com>
2022-07-19 11:13:22 -0700

    Task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,7 +8,7 @@ class Square:
 
     def __init__(self, size=0):
         """Initialize instance data"""
-        if not size.is_integer():
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
