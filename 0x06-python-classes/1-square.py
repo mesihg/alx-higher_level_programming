9819eb76768c1aa9a12eeb7bb16728d35fb21593
Mesih Grmay <mesihgrmay@yahoo.com>
2022-07-19 10:47:52 -0700

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""A Square Entity"""
+
+
+class Square:
+    """Empty class to represent a square"""
+    def __init__(self):
+        """Initialize instance data"""
+        self.__size = 0
