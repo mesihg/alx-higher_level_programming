63dc477056ce4ddfdf655f3ac30bf6edf75f9bb7
Mesih Grmay <mesihgrmay@yahoo.com>
2022-07-19 13:07:34 -0700

    Task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -48,6 +48,7 @@ class Square:
         """prints the square with the character #"""
         if self.__size == 0:
             print()
+            return
 
         [print() for i in range(self.__position[1])]
         for i in range(0, self.__size):
