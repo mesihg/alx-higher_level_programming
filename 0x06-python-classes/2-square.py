e9e90d315c7d085040fe395e25e79d643673431c
Mesih Grmay <mesihgrmay@yahoo.com>
2022-07-19 11:09:27 -0700

    Task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""A Square Entity"""
+
+
+class Square:
+    """Empty class to represent a square"""
+
+    def __init__(self, size=0):
+        """Initialize instance data"""
+        if not size.is_integer():
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
