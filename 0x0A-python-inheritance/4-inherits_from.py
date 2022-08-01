f6642ba9874f8c6ac7089c733c3dc4508475eb21
Mesih Grmay <mesihgrmay@yahoo.com>
2022-08-01 06:27:55 -0700

    Task 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""Check if an object is a subclass of a given class"""
+
+
+def inherits_from(obj, a_class):
+    """check if an object is a subclass of a given class
+
+    Args:
+       obj (any): The object to be checked
+       a_class (type): The class to be check for obj subclass
+
+    Returns:
+       True if the obj is a subclass of a_class False otherwise
+    """
+    return issubclass(obj, a_class)
