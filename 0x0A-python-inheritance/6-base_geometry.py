a202b86678ccc327b83bad83db72bbafea2bfa33
Mesih Grmay <mesihgrmay@yahoo.com>
2022-08-01 06:44:33 -0700

    Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""A geometry module"""
+
+
+class BaseGeometry:
+    """A class with public instance method: area(self)"""
+    def area(self):
+        """raises an exception with a message"""
+        raise Exception("area() is not implemented")
