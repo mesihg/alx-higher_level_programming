bc17a05c4680786579f37bde2c7ece84804ec8f2
Mesih Grmay <mesihgrmay@yahoo.com>
2022-08-02 08:33:29 -0700

    Task 9

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""Student class module"""
+
+
+class Student:
+    """Represents a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """initialize student class with data"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """returns a dictionary description of student class"""
+        return Student().__dict__
