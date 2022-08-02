19e4dbdafcc384b6f7f0256e5045cfe68195726d
Mesih Grmay <mesihgrmay@yahoo.com>
2022-08-02 08:47:47 -0700

    Task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""Student class module"""
+
+
+class Student:
+    """Represents a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """initialize student class with data"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """returns a dictionary description of student
+        instance with the given attrs"""
+        if attrs is None:
+            return self.__dict__
+        new_dict = {}
+        for attr in attrs:
+            new_dict[attr] = self.__dict__[attr]
+
+        return new_dict
