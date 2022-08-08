6f9a9f9faf835fa462a4b0a838ef7a7acc3e118e
Mesih Grmay <mesihgrmay@yahoo.com>
2022-08-08 15:00:51 -0700

    bug fix

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,6 +9,7 @@ class Square(Rectangle):
     """A square class that extends Rectangle class"""
 
     def __init__(self, size, x=0, y=0, id=None):
+        """Initialize a Square instance with a new data"""
         super().__init__(size, size, x, y, id)
 
     @property
