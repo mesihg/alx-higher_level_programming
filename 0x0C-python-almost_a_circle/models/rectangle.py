ed2fec50a99a42ff168fef73716fd4b8813f5594
Mesih Grmay <mesihgrmay@yahoo.com>
2022-08-08 14:50:06 -0700

    bug fix

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -23,7 +23,7 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """sets width of a rectangle instance"""
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
@@ -37,7 +37,7 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """set height of a rectangle instance"""
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
@@ -51,7 +51,7 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """set x attribute of a rectangle instance"""
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("x must be an integer")
         if value < 0:
             raise ValueError("x must be >= 0")
@@ -65,7 +65,7 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         """set y attribute of a rectangle instance"""
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("y must be an integer")
         if value < 0:
             raise ValueError("y must be >= 0")
@@ -80,15 +80,6 @@ class Rectangle(Base):
         if self.width == 0 or self.height:
             print()
             return
-        for i in range(self.height):
-            [print("#", end="") for w in range(self.width)]
-            print("")
-
-    def display1(self):
-        """prints the rectangle instance with character #"""
-        if self.width == 0 or self.height:
-            print()
-            return
         [print("") for y in range(self.y)]
         for i in range(self.height):
             [print(" ", end="") for x in range(self.x)]
@@ -97,29 +88,28 @@ class Rectangle(Base):
 
     def __str__(self):
         """returns string representation of the object"""
-        rect =
         return "[Rectangle] ({}) {}/{} - {}/{}".format(
             self.id, self.x, self.y, self.width, self.height)
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """Update each attributes of Rectangle class"""
-        i = 0
-        if len(args) != 0:
-            for key in self.__dict__:
-                if len(args) <= i:
-                    break
-                self.__dict__[key] = args[i]
-                i += 1
-
-    def update1(self, *args, **kwargs):
-        """Update each attributes of Rectangle class"""
-        i = 0
-        if len(args) != 0:
-            for key in self.__dict__:
-                if len(args) <= i:
-                    break
-                self.__dict__[key] = args[i]
-                i += 1
+        if args and len(args) != 0:
+            a = 0
+            for arg in args:
+                if a == 0:
+                    if arg is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif a == 1:
+                    self.width = arg
+                elif a == 2:
+                    self.height = arg
+                elif a == 3:
+                    self.x = arg
+                elif a == 4:
+                    self.y = arg
+                a += 1
         elif kwargs is not None:
             for k, val in kwargs.items():
                 setattr(self, k, val)
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,18 +9,18 @@ class Square(Rectangle):
     """A square class that extends Rectangle class"""
 
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(id, size, size, x, y)
+        super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
         """returns size of a square instance"""
-        return self.size
+        return self.width
 
     @size.setter
     def size(self, value):
         """set size of a square instance"""
-        self.height = value
         self.width = value
+        self.height = value
 
     def __str__(self):
         """returns string representation of the object"""
@@ -29,13 +29,21 @@ class Square(Rectangle):
 
     def update(self, *args, **kwargs):
         """Update each attributes of Square class"""
-        sqr_args = ["id", "height", "x", "y"]
-        args_len = len(args)
-        if len(args) != 0:
-            if args_len > len(sqr_args):
-                args_len = len(sqr_args)
-            for i in range(args_len):
-                setattr(self, sqr_args[i], args[i])
+        if args and len(args) != 0:
+            a = 0
+            for arg in args:
+                if a == 0:
+                    if arg is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif a == 1:
+                    self.size = arg
+                elif a == 2:
+                    self.x = arg
+                elif a == 3:
+                    self.y = arg
+                a += 1
         elif kwargs is not None:
             for k in kwargs:
                 setattr(self, k, kwargs[k])
