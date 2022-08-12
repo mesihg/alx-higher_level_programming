3997530d154fb416003bb604b41edf10e8a3e7eb
Mesih Grmay <mesihgrmay@yahoo.com>
2022-08-12 14:28:36 -0700

    bug fix

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -2,19 +2,263 @@
 
 """A test cases for rectangle module."""
 
-
+import io
+import os
+import sys
 import unittest
 from models.base import Base
 from models.rectangle import Rectangle
 
 
-class TestRectangle_instantiation(unittest.TestCase):
+class TestRectangle(unittest.TestCase):
     """Unittests for the Rectangle class."""
 
-    def test_rectangle(self):
+    @staticmethod
+    def capture_stdout(rect, method):
+        """Captures and returns text printed to stdout."""
+        capture = io.StringIO()
+        sys.stdout = capture
+        if method == "print":
+            print(rect)
+        else:
+            rect.display()
+        sys.stdout = sys.__stdout__
+        return capture
+
+    def test_rectangle_no_args(self):
+        with self.assertRaises(TypeError):
+            Rectangle()
+
+    def test_rectangle_one_args(self):
+        with self.assertRaises(TypeError):
+            Rectangle(1)
+
+    def test_rectangle_two_args(self):
         rect = Rectangle(1, 2)
-        self.assertEqual()
-   
+        self.assertIsInstance(rect, Base)
+
+    def test_rectangle_three_args(self):
+        rect = Rectangle(1, 2, 3)
+        self.assertIsInstance(rect, Base)
+
+    def test_rectangle_four_args(self):
+        rect = Rectangle(1, 2, 3, 4)
+        self.assertIsInstance(rect, Base)
+
+    def test_rectangle_five_args(self):
+        rect = Rectangle(1, 2, 3, 4, 6)
+        self.assertIsInstance(rect, Base)
+
+    def test_rectangle_string_width(self):
+        with self.assertRaisesRegex(TypeError, "width must be an integer"):
+            Rectangle("1", 4)
+
+    def test_rectangle_string_height(self):
+        with self.assertRaisesRegex(TypeError, "height must be an integer"):
+            Rectangle(1, "4")
+
+    def test_rectangle_string_x(self):
+        with self.assertRaisesRegex(TypeError, "x must be an integer"):
+            Rectangle(1, 3, "4")
+
+    def test_rectangle_string_y(self):
+        with self.assertRaisesRegex(TypeError, "y must be an integer"):
+            Rectangle(1, 3, 5, "4")
+
+    def test_rectangle_negative_width(self):
+        with self.assertRaisesRegex(ValueError, "width must be > 0"):
+            Rectangle(-1, 2)
+
+    def test_rectangle_negative_height(self):
+        with self.assertRaisesRegex(ValueError, "height must be > 0"):
+            Rectangle(1, -2)
+
+    def test_rectangle_zero_width(self):
+        with self.assertRaisesRegex(ValueError, "width must be > 0"):
+            Rectangle(0, 2)
+
+    def test_rectangle_zero_height(self):
+        with self.assertRaisesRegex(ValueError, "height must be > 0"):
+            Rectangle(1, 0)
+
+    def test_rectangle_negative_x(self):
+        with self.assertRaisesRegex(ValueError, "x must be >= 0"):
+            Rectangle(1, 3, -4)
+
+    def test_rectangle_negative_y(self):
+        with self.assertRaisesRegex(ValueError, "y must be >= 0"):
+            Rectangle(1, 3, 5, -4)
+
+    def test_rectangle_area_valid(self):
+        rect = Rectangle(10, 20, 3, 4, 5)
+        self.assertEqual(200, rect.area())
+
+    def test_rectangle_area_args(self):
+        rect = Rectangle(10, 20, 3, 4, 5)
+        with self.assertRaises(TypeError):
+            rect.area('10')
+
+    def test_rectangle_str_all_valid(self):
+        rect = Rectangle(10, 20, 3, 4, 5)
+        test = "[Rectangle] ({}) {}/{} - {}/{}".format(
+            rect.id, rect.x, rect.y, rect.width, rect.height)
+        self.assertEqual(test, rect.__str__())
+
+    def test_rectangle_str_width_height_new_attributes(self):
+        rect = Rectangle(10, 20, 5, 6, 9)
+        rect.width = 5
+        rect.height = 19
+        test = "[Rectangle] (9) 5/6 - 5/19"
+        self.assertEqual(test, rect.__str__())
+
+    def test_rectangle_str_args(self):
+        rect = Rectangle(10, 20)
+        with self.assertRaises(TypeError):
+            rect.__str__(5, 5)
+
+    def test_rectangle_display_x(self):
+        rect = Rectangle(3, 2, 1, 0, 1)
+        capture = TestRectangle.capture_stdout(rect, "display")
+        self.assertEqual(" ###\n ###\n", capture.getvalue())
+
+    def test_rectangle_display_y(self):
+        rect = Rectangle(4, 5, 0, 1, 0)
+        capture = TestRectangle.capture_stdout(rect, "display")
+        display = "\n####\n####\n####\n####\n####\n"
+        self.assertEqual(display, capture.getvalue())
+
+    def test_rectangle_display_no_x_y(self):
+        rect = Rectangle(2, 3, 0, 0, 0)
+        capture = TestRectangle.capture_stdout(rect, "display")
+        self.assertEqual("##\n##\n##\n", capture.getvalue())
+
+    def test_rectangle_display_x_y(self):
+        rect = Rectangle(2, 4, 3, 2, 0)
+        capture = TestRectangle.capture_stdout(rect, "display")
+        display = "\n\n   ##\n   ##\n   ##\n   ##\n"
+        self.assertEqual(display, capture.getvalue())
+
+    def test_rectangle_display_args(self):
+        rect = Rectangle(2, 4, 3, 2, 0)
+        with self.assertRaises(TypeError):
+            rect.display(1, 2)
+
+    def test_rectangle_to_dictionary(self):
+        rect = Rectangle(2, 3, 5, 6, 7)
+        dict_test = {'x': 5, 'y': 6, 'id': 7, 'height': 3, 'width': 2}
+        self.assertDictEqual(dict_test, rect.to_dictionary())
+
+    def test_rectangle_update_0(self):
+        rect = Rectangle(10, 10, 10, 10, 10)
+        rect.update()
+        self.assertEqual("[Rectangle] (10) 10/10 - 10/10", str(rect))
+
+    def test_rectangle_update_1(self):
+        rect = Rectangle(10, 10, 10, 10, 10)
+        rect.update(89)
+        self.assertEqual("[Rectangle] (89) 10/10 - 10/10", str(rect))
+
+    def test_rectangle_update_2(self):
+        rect = Rectangle(10, 10, 10, 10, 10)
+        rect.update(6, 98)
+        self.assertEqual("[Rectangle] (6) 10/10 - 98/10", str(rect))
+
+    def test_rectangle_update_3(self):
+        rect = Rectangle(10, 10, 10, 10, 10)
+        rect.update(6, 98, 1)
+        self.assertEqual("[Rectangle] (6) 10/10 - 98/1", str(rect))
+
+    def test_rectangle_update_4(self):
+        rect = Rectangle(10, 10, 10, 10, 10)
+        rect.update(6, 98, 1, 1)
+        self.assertEqual("[Rectangle] (6) 1/10 - 98/1", str(rect))
+
+    def test_rectangle_update_5(self):
+        rect = Rectangle(10, 10, 10, 10, 10)
+        rect.update(**{'id': 90})
+        self.assertEqual("[Rectangle] (90) 10/10 - 10/10", str(rect))
+
+    def test_rectangle_update_6(self):
+        rect = Rectangle(10, 10, 10, 10, 10)
+        rect.update(**{'id': 90, 'width': 1})
+        self.assertEqual("[Rectangle] (90) 10/10 - 1/10", str(rect))
+
+    def test_rectangle_update_7(self):
+        rect = Rectangle(10, 10, 10, 10, 10)
+        rect.update(**{'id': 89, 'width': 1, 'height': 2})
+        self.assertEqual("[Rectangle] (89) 10/10 - 1/2", str(rect))
+
+    def test_rectangle_update_8(self):
+        rect = Rectangle(10, 10, 10, 10, 10)
+        rect.update(**{'id': 89, 'width': 1, 'height': 2})
+        self.assertEqual("[Rectangle] (89) 10/10 - 1/2", str(rect))
+
+    def test_rectangle_update_9(self):
+        rect = Rectangle(10, 10, 10, 10, 10)
+        rect.update(**{'id': 89, 'width': 1, 'height': 2, 'x': 3})
+        self.assertEqual("[Rectangle] (89) 3/10 - 1/2", str(rect))
+
+    def test_rectangle_update_10(self):
+        rect = Rectangle(10, 10, 10, 10, 10)
+        rect.update(**{'id': 89, 'width': 1, 'height': 2, 'x': 3, 'y': 4})
+        self.assertEqual("[Rectangle] (89) 3/4 - 1/2", str(rect))
+
+    def test_rectangle_update_11(self):
+        rect = Rectangle(10, 10, 10, 10, 10)
+        rect.update(**{'id': 89, 'width': 1, 'height': 2, 'x': 3, 'y': 4})
+        self.assertEqual("[Rectangle] (89) 3/4 - 1/2", str(rect))
+
+    def test_rectangle_create0(self):
+        rect = Rectangle.create(**{'id': 89})
+        self.assertEqual("[Rectangle] (89) 0/0 - 1/1", str(rect))
+
+    def test_rectangle_create1(self):
+        rect = Rectangle.create(**{'id': 89, 'width': 1})
+        self.assertEqual("[Rectangle] (89) 0/0 - 1/1", str(rect))
+
+    def test_rectangle_create2(self):
+        rect = Rectangle.create(**{'id': 89, 'width': 1, 'height': 2})
+        self.assertEqual("[Rectangle] (89) 0/0 - 1/2", str(rect))
+
+    def test_rectangle_create3(self):
+        rect = Rectangle.create(**{'id': 89, 'width': 1, 'height': 2})
+        self.assertEqual("[Rectangle] (89) 0/0 - 1/2", str(rect))
+
+    def test_rectangle_create4(self):
+        rect = Rectangle.create(**{'id': 89, 'width': 1, 'height': 2, 'x': 3})
+        self.assertEqual("[Rectangle] (89) 3/0 - 1/2", str(rect))
+
+    def test_rectangle_create5(self):
+        rect = Rectangle.create(
+            **{'id': 89, 'width': 1, 'height': 2, 'x': 3, 'y': 4})
+        self.assertEqual("[Rectangle] (89) 3/4 - 1/2", str(rect))
+
+    def test_rectangle_save_to_file_none(self):
+        Rectangle.save_to_file(None)
+        output = Rectangle.load_from_file()
+        self.assertEqual(output, [])
+
+    def test_rectangle_save_to_file_empty_list(self):
+        Rectangle.save_to_file([])
+        output = Rectangle.load_from_file()
+        self.assertEqual(output, [])
+
+    def test_rectangle_save_to_file_list(self):
+        Rectangle.save_to_file([Rectangle(1, 2)])
+        with open("Rectangle.json", "r") as f:
+            self.assertTrue(os.path.exists("Rectangle.json"), True)
+
+    def test_rectangle_load_from_file_no_file(self):
+        if os.path.exists("Rectangle.json"):
+            os.remove("Rectangle.json")
+        list_rectangles_output = Rectangle.load_from_file()
+        self.assertEqual(list_rectangles_output, [])
+
+    def test_rectangle_load_from_file(self):
+        Rectangle.save_to_file([Rectangle(1, 2)])
+        with open("Rectangle.json", "r") as f:
+            self.assertTrue(os.path.exists("Rectangle.json"), True)
+
 
 if __name__ == "__main__":
     unittest.main()
--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -4,12 +4,161 @@
 
 
 import unittest
+import os
 from models.base import Base
 from models.square import Square
 
 
 class TestSquare(unittest.TestCase):
     """Unittests for the Square class."""
+
+    def test_square_no_args(self):
+        with self.assertRaises(TypeError):
+            Square()
+
+    def test_square_two_args(self):
+        sqr = Square(1, 2)
+        self.assertIsInstance(sqr, Base)
+
+    def test_square_three_args(self):
+        sqr = Square(1, 2, 3)
+        self.assertIsInstance(sqr, Base)
+
+    def test_square_string_size(self):
+        with self.assertRaisesRegex(TypeError, "width must be an integer"):
+            Square("1")
+
+    def test_square_string_x(self):
+        with self.assertRaisesRegex(TypeError, "x must be an integer"):
+            Square(1, "2")
+
+    def test_square_string_y(self):
+        with self.assertRaisesRegex(TypeError, "y must be an integer"):
+            Square(1, 2, "3")
+
+    def test_square_four_args(self):
+        sqr = Square(1, 2, 3, 4)
+        self.assertIsInstance(sqr, Base)
+
+    def test_square_negative_width(self):
+        with self.assertRaisesRegex(ValueError, "width must be > 0"):
+            Square(-1)
+
+    def test_square_negative_x(self):
+        with self.assertRaisesRegex(ValueError, "x must be >= 0"):
+            Square(1, -2)
+
+    def test_square_negative_y(self):
+        with self.assertRaisesRegex(ValueError, "y must be >= 0"):
+            Square(1, 2, -3)
+
+    def test_square_zero(self):
+        with self.assertRaisesRegex(ValueError, "width must be > 0"):
+            Square(0)
+
+    def test_square_str_all_valid(self):
+        sqr = Square(10, 20, 3, 4)
+        test = "[Square] ({}) {}/{} - {}".format(
+            sqr.id, sqr.x, sqr.y, sqr.width)
+        self.assertEqual(test, sqr.__str__())
+
+    def test_square_str_size_new_attributes(self):
+        sqr = Square(10, 20, 5, 6)
+        sqr.size = 5
+        test = "[Square] (6) 20/5 - 5"
+        self.assertEqual(test, sqr.__str__())
+
+    def test_square_to_dictionary(self):
+        sqr = Square(2, 3, 5, 6)
+        dict_test = {'x': 3, 'y': 5, 'id': 6, 'size': 2}
+        self.assertDictEqual(dict_test, sqr.to_dictionary())
+
+    def test_square_update_0(self):
+        sqr = Square(10, 10, 10, 10)
+        sqr.update()
+        self.assertEqual("[Square] (10) 10/10 - 10", str(sqr))
+
+    def test_square_update_1(self):
+        sqr = Square(10, 10, 10, 10)
+        sqr.update(89)
+        self.assertEqual("[Square] (89) 10/10 - 10", str(sqr))
+
+    def test_square_update_2(self):
+        sqr = Square(10, 10, 10, 10)
+        sqr.update(89, 1)
+        self.assertEqual("[Square] (89) 10/10 - 1", str(sqr))
+
+    def test_square_update_3(self):
+        sqr = Square(10, 10, 10, 10)
+        sqr.update(89, 1, 2)
+        self.assertEqual("[Square] (89) 2/10 - 1", str(sqr))
+
+    def test_square_update_4(self):
+        sqr = Square(10, 10, 10, 10)
+        sqr.update(89, 1, 2, 3)
+        self.assertEqual("[Square] (89) 2/3 - 1", str(sqr))
+
+    def test_square_update_5(self):
+        sqr = Square(10, 10, 10, 10)
+        sqr.update(**{'id': 90})
+        self.assertEqual("[Square] (90) 10/10 - 10", str(sqr))
+
+    def test_square_update_6(self):
+        sqr = Square(10, 10, 10, 10)
+        sqr.update(**{'id': 87, 'size': 1})
+        self.assertEqual("[Square] (87) 10/10 - 1", str(sqr))
+
+    def test_square_update_7(self):
+        sqr = Square(10, 10, 10, 10)
+        sqr.update(**{'id': 80, 'size': 1, 'x': 2})
+        self.assertEqual("[Square] (80) 2/10 - 1", str(sqr))
+
+    def test_square_update_8(self):
+        sqr = Square(10, 10, 10, 10)
+        sqr.update(**{'id': 81, 'size': 1, 'x': 2, 'y': 3})
+        self.assertEqual("[Square] (81) 2/3 - 1", str(sqr))
+
+    def test_square_create0(self):
+        sqr = Square.create(**{'id': 82})
+        self.assertEqual("[Square] (82) 0/0 - 1", str(sqr))
+
+    def test_square_create1(self):
+        sqr = Square.create(**{'id': 89, 'size': 1})
+        self.assertEqual("[Square] (89) 0/0 - 1", str(sqr))
+
+    def test_square_create2(self):
+        sqr = Square.create(**{'id': 89, 'size': 1, 'x': 2})
+        self.assertEqual("[Square] (89) 2/0 - 1", str(sqr))
+
+    def test_square_create3(self):
+        sqr = Square.create(**{'id': 89, 'size': 1, 'x': 2, 'y': 3})
+        self.assertEqual("[Square] (89) 2/3 - 1", str(sqr))
+
+    def test_square_save_to_file_none(self):
+        Square.save_to_file(None)
+        output = Square.load_from_file()
+        self.assertEqual(output, [])
+
+    def test_square_save_to_file_empty_list(self):
+        Square.save_to_file([])
+        output = Square.load_from_file()
+        self.assertEqual(output, [])
+
+    def test_square_save_to_file_list(self):
+        Square.save_to_file([Square(1)])
+        with open("Square.json", "r") as f:
+            self.assertTrue(os.path.exists("Square.json"), True)
+
+    def test_square_load_from_file_no_file(self):
+        if os.path.exists("Square.json"):
+            os.remove("Square.json")
+        list_squares_output = Square.load_from_file()
+        self.assertEqual(list_squares_output, [])
+
+    def test_square_load_from_file(self):
+        Square.save_to_file([Square(1)])
+        with open("Square.json", "r") as f:
+            self.assertTrue(os.path.exists("Square.json"), True)
 
 
 if __name__ == "__main__":
