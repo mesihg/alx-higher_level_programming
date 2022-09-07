f5ad4ff7bfdced37ebdf4abdbf141aede36ef9ff
Messih Grmay <mesihgrmay@yahoo.com>
2022-09-07 12:10:07 -0700

    Task 19

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,5 +1,5 @@
 -- lists all shows from hbtn_0d_tvshows_rate by their rating
-SELECT A.title, SUM(B.rating) AS rating FROM tv_shows A
+SELECT A.title, SUM(B.rate) AS rating FROM tv_shows A
 INNER JOIN tv_show_ratings B
 ON A.id = B.show_id
 GROUP BY A.title
