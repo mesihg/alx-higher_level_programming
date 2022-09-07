4ad1ddffada273cb3e0f63d4c54d638afa0abcb6
Messih Grmay <mesihgrmay@yahoo.com>
2022-09-07 10:21:00 -0700

    Task 13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,7 +3,7 @@
 SELECT A.name AS genre,
       COUNT(*) AS number_of_shows
 FROM tv_genres AS A
-LEFT JOIN tv_show_genres AS B
+INNER JOIN tv_show_genres AS B
 ON A.id = B.genre_id
 GROUP BY A.name
 ORDER BY number_of_shows DESC;
