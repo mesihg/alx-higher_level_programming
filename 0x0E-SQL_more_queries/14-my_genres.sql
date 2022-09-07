53dc2fec81892e149f5eaa718e4595eab1d970cd
Messih Grmay <mesihgrmay@yahoo.com>
2022-09-07 10:44:29 -0700

    Task 14

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,8 +1,9 @@
--- List all shows contained in hbtn_0d_tvshows 
--- and displays the number of shows linked to each
+--  lists all genres of the show Dexter
 SELECT A.name
 FROM tv_genres AS A
-INNER JOIN tv_shows AS B
-ON A.show_id = B.id
-AND B.title = "Dexter"
+INNER JOIN tv_show_genres B
+ON A.id = B.genre_id
+INNER JOIN tv_shows AS C
+ON B.show_id = C.id
+AND C.title = "Dexter"
 ORDER BY A.name;
