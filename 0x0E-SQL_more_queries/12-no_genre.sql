b698d696f43d1f7147329ec896ecc591234378ea
Messih Grmay <mesihgrmay@yahoo.com>
2022-09-07 10:02:30 -0700

    Task 12

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,8 @@
+-- List all shows contained in hbtn_0d_tvshows
+SELECT A.title,
+      B.genre_id
+FROM tv_shows AS A
+LEFT JOIN tv_show_genres AS B
+ON A.id = B.show_id
+WHERE B.show_id IS NULLL
+ORDER BY A.title, B.genre_id;
