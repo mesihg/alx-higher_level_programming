d7767e917def35ad487576da45edd049875b27e0
Messih Grmay <mesihgrmay@yahoo.com>
2022-09-07 10:54:31 -0700

    Task 16

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,9 @@
+--  lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows
+SELECT A.title,
+       C.name
+FROM tv_shows AS A
+LEFT JOIN tv_show_genres B
+ON A.id = B.show_id
+LEFT JOIN tv_genres AS C
+ON B.genre_id = C.id
+ORDER BY A.title, C.name;
