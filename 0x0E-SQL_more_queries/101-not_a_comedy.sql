3a5f6540632468a73740cf9d1e1e77add9ccf8e5
Messih Grmay <mesihgrmay@yahoo.com>
2022-09-07 11:53:30 -0700

    Task 18

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,7 +1,7 @@
 -- lists all shows without the genre Comedy in the database hbtn_0d_tvshows.
 SELECT A.title
 FROM tv_shows AS A
-WHERE A.id IN (
+WHERE A.id NOT IN (
  SELECT B.show_id FROM tv_show_genres B
 INNER JOIN tv_genres C
 ON B.genre_id = C.id
