a552c05ffec4276219da945ac60943a09d0c31d8
Messih Grmay <mesihgrmay@yahoo.com>
2022-09-07 09:52:06 -0700

    Task 10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,7 @@
 -- List all shows contained in hbtn_0d_tvshows
 SELECT tv_show.title AS title,
 tv_show_genres.genre_id AS genre_id
-FROM tv_shows
-INNER JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id;
+FROM tv_shows 
+INNER JOIN tv_show_genres 
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
