9f5e4bd52994379cbcde0d1c77c6afc8106ccd38
Messih Grmay <mesihgrmay@yahoo.com>
2022-09-07 10:50:05 -0700

    Task 15

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -3,7 +3,7 @@ SELECT A.title
 FROM tv_shows AS A
 INNER JOIN tv_show_genres B
 ON A.id = B.show_id
-INNER JOIN tv_genre AS C
+INNER JOIN tv_genres AS C
 ON B.genre_id = C.id
 AND C.name = "Comedy"
 ORDER BY A.title;
