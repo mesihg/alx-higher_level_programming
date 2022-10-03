8582f1febcc688201258a265e1de580c2ee70b9f
Messih Grmay <mesihgrmay@yahoo.com>
2022-10-03 12:15:03 -0700

    Task 11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+if (process.argv.length <= 3) {
+  console.log(0);
+} else {
+  const numbers = process.argv.slice(2, process.argv.length).sort((a, b) => a - b);
+  console.log(numbers[numbers.length - 2]);
+}
