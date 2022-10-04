9179c947d8f6a560e7e3bf72935ef406e71cdec2
Messih Grmay <mesihgrmay@yahoo.com>
2022-10-04 06:03:31 -0700

    Task 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) return;
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let stringX = '';
+      for (let j = 0; j < this.width; j++) {
+        stringX += 'X';
+      }
+      console.log(stringX);
+    }
+  }
+
+  rotate () {
+    const temp = this.height;
+    this.width = temp;
+    this.height = this.width;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
