7f766e27d1bbc29bb1738fbddfc9b25fb38fee84
Messih Grmay <mesihgrmay@yahoo.com>
2022-10-04 06:06:10 -0700

    Task 4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -18,9 +18,7 @@ module.exports = class Rectangle {
   }
 
   rotate () {
-    const temp = this.height;
-    this.width = temp;
-    this.height = this.width;
+    [this.width, this.height] = [this.height, this.width];
   }
 
   double () {
