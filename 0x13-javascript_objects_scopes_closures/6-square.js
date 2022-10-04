e1e4f75592440bc0133ff872a6ba49466b01666b
Messih Grmay <mesihgrmay@yahoo.com>
2022-10-04 11:55:30 -0700

    Task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,7 +3,7 @@ const Square1 = require('./5-square.js');
 
 module.exports = class Square extends Square1 {
   charPrint (c) {
-    if (c !== 'undefined') {
+    if (c !== undefined) {
       for (let i = 0; i < this.height; i++) {
         let stringX = '';
         for (let j = 0; j < this.width; j++) {
