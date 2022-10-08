2a078063ee4586957831a8d3830d446dcae8cfa0
Messih Grmay <mesihgrmay@yahoo.com>
2022-10-08 14:50:24 -0700

    Task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa
+lists all states with a name starting with N (upper N)
 """
 import sys
 import MySQLdb
