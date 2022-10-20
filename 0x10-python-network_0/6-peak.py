3fb56bec52f12fbc8fc69f547aa5f2d92dd8a49c
Messih Grmay <mesihgrmay@yahoo.com>
2022-10-20 13:05:20 -0700

    Task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""A function that finds a peak in a list of unsorted integers"""
+
+
+def find_peak(list_of_integers):
+    """finds a peak in a list of unsorted integers"""
+
+    if len(list_of_integers) == 0:
+        return None
+    left, right = 0, len(list_of_integers) - 1
+    while left < right:
+        mid = (left + right)//2
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            right = mid
+        else:
+            left = mid + 1
+    return list_of_integers[left]
