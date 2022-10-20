b7cacb0cdf4be796884caf5fb8fca7a563f5d03e
Messih Grmay <mesihgrmay@yahoo.com>
2022-10-20 12:51:10 -0700

    Task 5

---
+++ 0x10-python-network_0/5-post_params.sh
@@ -0,0 +1,3 @@
+#!/bin/bash 
+# Send a POST request to the passed URL and displays the body of the response
+curl -s -X POST -F "email=test@gmail.com" -F "subject=I will always be here for PLD" "$1"
