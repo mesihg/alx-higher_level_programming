28a6a6200e34341c2d21751127415ccf9bf4c73a
Messih Grmay <mesihgrmay@yahoo.com>
2022-10-21 06:08:16 -0700

    Task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,5 +5,5 @@ import requests
 if __name__ == '__main__':
     resp_data = requests.get('https://alx-intranet.hbtn.io/status')
     print("Body response:")
-    print("\t- type: {}".format(type(resp_data)))
+    print("\t- type: {}".format(type(resp_data.text)))
     print("\t- content: {}".format(resp_data.text))
