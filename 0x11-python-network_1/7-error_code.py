2ae7bca283f4016c23ecaf6e1663ce2f15c2c8f8
Messih Grmay <mesihgrmay@yahoo.com>
2022-10-21 06:43:11 -0700

    Task 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" "Make a request and display errors if any""" 
+import requests
+import sys
+
+if __name__ == '__main__':
+    resp_data = requests.get(sys.argv[1])
+    if (resp_data.status_code >= 400):
+        print("Error code: {}".format(resp_data.status_code))
