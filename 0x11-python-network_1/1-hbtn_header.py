4877bb17c2b4299cab96bb3f971de500902cb673
Messih Grmay <mesihgrmay@yahoo.com>
2022-10-21 04:19:59 -0700

    Task 1

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Make a request to a URL and displays the value of the X-Request-Id """
+import urllib.request
+import sys
+
+if __name__ == '__main__':
+    request = urllib.request.Request(sys.argv[1])
+    with urllib.request.urlopen(request) as resp:
+        print(dict(resp.headers).get('X-Request_id'))
