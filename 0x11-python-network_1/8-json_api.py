19a59c5db5786cfd598cf55cd9018a8800087fce
Messih Grmay <mesihgrmay@yahoo.com>
2022-10-21 10:42:24 -0700

    Task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -4,13 +4,13 @@ import requests
 import sys
 
 if __name__ == '__main__':
-    if len(sys.argv[1]) == 1:
-        search_param = {'q': sys.argv[1]}
+    if len(sys.argv) == 1:
+        aram = ""
     else:
-        search_param = {'q': ""}
-    resp_data = requests.post("http://0.0.0.0:5000/search_user", data=search_param)
+        param = sys.argv[1]
+    resp = requests.post("http://0.0.0.0:5000/search_user", data={'q': param})
     try:
-        response = resp_data.json()
+        response = resp.json()
         if response == {}:
             print('No result')
         else:
