89c5c300b2b7f0fd4765c6a4769fc468b2d8f243
Messih Grmay <mesihgrmay@yahoo.com>
2022-10-21 05:51:32 -0700

    Task 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Make a request and handle HTTPError """
+import urllib.request
+import sys
+
+if __name__ == '__main__':
+    try:
+        req = urllib.request.Request(sys.argv[1])
+        with urllib.request.urlopen(req) as response:
+            body = response.read()
+            print(body.decode('utf8'))
+    except urlllib.error.HTTPError as err:
+        print("Error code: {}".format(err.code))
