a122c04dbbb96696183da5d223fc9f57be3177d1
Messih Grmay <mesihgrmay@yahoo.com>
2022-10-21 06:33:43 -0700

    Task 5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """ Fetch data using request and displays the value of X-Request-Id """
 import requests
+import sys
 
 if __name__ == '__main__':
-    resp_data = requests.get('https://alx-intranet.hbtn.io/status')
+    resp_data = requests.get(sys.argv[1])
     print(resp_data.headers['X-Request-Id'])
