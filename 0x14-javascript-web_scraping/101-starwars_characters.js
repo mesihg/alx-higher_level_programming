ed82df91d924489c2c744d4a827cfaf25232b2b0
Messih Grmay <mesihgrmay@yahoo.com>
2022-11-15 12:20:32 -0800

    Task 8

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,29 +1,20 @@
 #!/usr/bin/node
-
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/';
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
+  }
+});
 
-function characterRequest (characterUrl) {
-  return new Promise((resolve, reject) => {
-    request.get(characterUrl, (err, _resp, body) => {
-      if (err !== null) {
-        reject(err);
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
       }
-      resolve(body);
-    });
+    }
   });
 }
-
-request(url + process.argv[2], function (err, res, body) {
-  if (err) {
-    console.log(err);
-  }
-  const film = JSON.parse(body);
-  const characters = film.characters;
-  characters.forEach((character) => {
-    characterRequest(character).then((body) => {
-      const characterInfo = JSON.parse(body);
-      console.log(characterInfo.name);
-    }).catch(err => console.log(err));
-  });
-});
