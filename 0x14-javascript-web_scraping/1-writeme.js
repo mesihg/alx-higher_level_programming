4e304f6ecd5df32f48c7b4ef558a24a931edd6e4
Messih Grmay <mesihgrmay@yahoo.com>
2022-11-15 10:12:11 -0800

    Task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,10 +2,8 @@
 
 const fs = require('fs');
 
-fs.writeFile(process.argv[2], process.argv[3], 'utf-8', function (err, data) {
+fs.writeFile(process.argv[2], process.argv[3], 'utf-8', function (err) {
   if (err) {
     console.log(err);
-  } else {
-    console.log(data);
   }
 });
