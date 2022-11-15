5c0934f295a1d3c19f97fbaa55b646468cd30caa
Messih Grmay <mesihgrmay@yahoo.com>
2022-11-15 11:23:48 -0800

    Task 5

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+
+request(process.argv[2], function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    fs.writeFile(process.argv[3], body, 'utf-8');
+  }
+});
