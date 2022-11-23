12140074004c1d860e79194d60d085c78e2b7062
Messih Grmay <mesihgrmay@yahoo.com>
2022-11-23 05:40:32 -0800

    Task 2

--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -1,5 +1,5 @@
 const header = document.querySelector('header');
-const div_tag = document.querySelector('DIV#red_header');
-div_tag.addEventListener('click', function(){
-        header.style.color = '#FF0000';
+const divTag = document.querySelector('DIV#red_header');
+divTag.addEventListener('click', function () {
+  header.style.color = '#FF0000';
 });
